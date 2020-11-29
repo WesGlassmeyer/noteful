6a03d0872b4df80753ce05b247f9b1db71df2401
Wes Glassmeyer <71471439+WesGlassmeyer@users.noreply.github.com>
2020-11-29 16:59:47 -0700

    Update NotePageNav.js

--- src/NotePageNav/NotePageNav.js
+++ src/NotePageNav/NotePageNav.js
@@ -1,40 +1,45 @@
 import React from "react";
 import CircleButton from "../CircleButton/CircleButton";
-import "./NotePageNav.css";
-<<<<<<< Updated upstream
-=======
 import ApiContext from "../ApiContext";
 import { findNote, findFolder } from "../notes-helpers";
+import "./NotePageNav.css";
 import PropTypes from "prop-types";
->>>>>>> Stashed changes
 
-export default function NotePageNav(props) {
-  return (
-    <div className="NotePageNav">
-      <CircleButton
-        tag="button"
-        role="link"
-        onClick={() => props.history.goBack()}
-        className="NotePageNav_back-button"
-      >
-        <br />
-        Back
-      </CircleButton>
-      {props.folder && (
-        <h3 className="NotePageNav_folder-name">{props.folder.name}</h3>
-      )}
-    </div>
-  );
+export default class NotePageNav extends React.Component {
+  static defaultProps = {
+    history: {
+      goBack: () => {},
+    },
+    match: {
+      params: {},
+    },
+  };
+  static contextType = ApiContext;
+
+  render() {
+    const { notes, folders } = this.context;
+    const { noteId } = this.props.match.params;
+    const note = findNote(notes, noteId) || {};
+    const folder = findFolder(folders, note.folderId);
+    return (
+      <div className="NotePageNav">
+        <CircleButton
+          tag="button"
+          role="link"
+          onClick={() => this.props.history.goBack()}
+          className="NotePageNav_back-button"
+        >
+          Go
+          <br />
+          Back
+        </CircleButton>
+        {folder && <h3 className="NotePageNav_folder-name">{folder.name}</h3>}
+      </div>
+    );
+  }
 }
 
-<<<<<<< Updated upstream
-NotePageNav.defaultProps = {
-  history: {
-    goBack: () => {},
-  },
-=======
 NotePageNav.propTypes = {
   history: PropTypes.object,
   match: PropTypes.object,
->>>>>>> Stashed changes
 };
