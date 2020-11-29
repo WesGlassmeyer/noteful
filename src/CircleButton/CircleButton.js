2d04348146b0c91cba7b78a550c0204cbc770616
Wes Glassmeyer <71471439+WesGlassmeyer@users.noreply.github.com>
2020-11-29 16:52:17 -0700

    Update CircleButton.js

--- src/CircleButton/CircleButton.js
+++ src/CircleButton/CircleButton.js
@@ -1,10 +1,6 @@
 import React from "react";
-<<<<<<< Updated upstream
-//import ".CircleButton.css";
-=======
 import "./CircleButton.css";
 import PropTypes from "prop-types";
->>>>>>> Stashed changes
 
 export default function NavCircleButton(props) {
   const { tag, className, children, ...otherProps } = props;
