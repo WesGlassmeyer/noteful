b9bd91c2fcaa306a48056eebec3e482a3c6884bc
Wes Glassmeyer <71471439+WesGlassmeyer@users.noreply.github.com>
2020-11-29 16:58:29 -0700

    Update NotePageMain.js

--- src/NotePageMain/NotePageMain.js
+++ src/NotePageMain/NotePageMain.js
@@ -1,39 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
 import Note from "../Note/Note";
-<<<<<<< Updated upstream
-//import "./NotePageMain.css";
-=======
-import "./NotePageMain.css";
 import ApiContext from "../ApiContext";
 import { findNote } from "../notes-helpers";
+import "./NotePageMain.css";
 import PropTypes from "prop-types";
->>>>>>> Stashed changes
 
-export default function NotePageMain(props) {
-  return (
-    <section className="NotePageMain">
-      <Note
-        id={props.note.id}
-        name={props.note.name}
-        modified={props.note.modified}
-      />
-      <div className="NotePageMain_content">
-        {props.note.content.split(/\n \r|\n/).map((para, i) => (
-          <p key={i}>{para}</p>
-        ))}
-      </div>
-    </section>
-  );
+export default class NotePageMain extends Component {
+  static defaultProps = {
+    match: {
+      params: {},
+    },
+  };
+  static contextType = ApiContext;
+
+  handleDeleteNote = (noteId) => {
+    this.props.history.push(`/`);
+  };
+
+  render() {
+    const { notes = [] } = this.context;
+    const { noteId } = this.props.match.params;
+    const note = findNote(notes, noteId) || { content: "" };
+    return (
+      <section className="NotePageMain">
+        <Note
+          id={note.id}
+          name={note.name}
+          modified={note.modified}
+          onDeleteNote={this.handleDeleteNote}
+        />
+        <div className="NotePageMain_content">
+          {note.content.split(/\n \r|\n/).map((para, i) => (
+            <p key={i}>{para}</p>
+          ))}
+        </div>
+      </section>
+    );
+  }
 }
 
-<<<<<<< Updated upstream
-NotePageMain.defaultProps = {
-  note: {
-    content: "",
-  },
-=======
 NotePageMain.propTypes = {
   history: PropTypes.object,
   match: PropTypes.object,
->>>>>>> Stashed changes
 };
