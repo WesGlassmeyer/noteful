ed58dff47473270ae16454b4bbbabeff67edc486
Wes Glassmeyer <71471439+WesGlassmeyer@users.noreply.github.com>
2020-11-29 16:55:27 -0700

    Update NoteListMain.js

--- src/NoteListMain/NoteListMain.js
+++ src/NoteListMain/NoteListMain.js
@@ -1,46 +1,56 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import Note from "../Note/Note";
 import CircleButton from "../CircleButton/CircleButton";
-<<<<<<< Updated upstream
-//import "./NoteListMain.css";
-=======
 import ApiContext from "../ApiContext";
 import { getNotesForFolder } from "../notes-helpers";
 import "./NoteListMain.css";
 import PropTypes from "prop-types";
->>>>>>> Stashed changes
 
-export default function NoteListMain(props) {
-  return (
-    <section className="NoteListMain">
-      <ul>
-        {props.notes.map((note) => (
-          <li key={note.id}>
-            <Note name={note.name} modified={note.modified} />
-          </li>
-        ))}
-      </ul>
-      <div className="NoteListMain_button-container">
-        <CircleButton
-          tag={Link}
-          to="/add-note"
-          type="button"
-          className="NoteListMain_add-note-button"
-        >
-          <br />
-          Note
-        </CircleButton>
-      </div>
-    </section>
-  );
+export default class NoteListMain extends Component {
+  static defaultProps = {
+    match: {
+      params: {},
+    },
+  };
+  static contextType = ApiContext;
+
+  render() {
+    const { folderId } = this.props.match.params;
+    const { notes = [] } = this.context;
+    const notesForFolder = getNotesForFolder(notes, folderId);
+    return (
+      <section className="NoteListMain">
+        <ul>
+          {notesForFolder.map((note) => (
+            <li key={note.id}>
+              <Note
+                id={note.id}
+                name={note.name}
+                modified={note.modified}
+                onDeleteNote={this.onDeleteNote}
+              />
+            </li>
+          ))}
+        </ul>
+        <div className="NoteListMain_button-container">
+          <CircleButton
+            tag={Link}
+            to="/add-note"
+            type="button"
+            className="NoteListMain_add-note-button"
+          >
+            <br></br>
+            Add
+            <br />
+            Note
+          </CircleButton>
+        </div>
+      </section>
+    );
+  }
 }
 
-<<<<<<< Updated upstream
-NoteListMain.defaultProps = {
-  notes: [],
-=======
 NoteListMain.propTypes = {
   match: PropTypes.object,
->>>>>>> Stashed changes
 };
