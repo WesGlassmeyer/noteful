35a67efce55d99217102874bbb7e74154c77b464
Wes Glassmeyer <71471439+WesGlassmeyer@users.noreply.github.com>
2020-11-29 16:53:56 -0700

    Update Note.js

--- src/Note/Note.js
+++ src/Note/Note.js
@@ -1,52 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
-<<<<<<< Updated upstream
-//import { format } from "date-fns";
-//import "./Note.css";
-
-export default function Note(props) {
-  return (
-    <div className="Note">
-      <h2 className="Note_title">
-        <Link to={`/note/${props.id}`}>{props.name}</Link>
-      </h2>
-      <button className="Note_delete" type="button">
-        {" "}
-        remove
-      </button>
-      <div className="Note_dates">
-        <div className="Note_dates-modified">
-          Modified <span className="Date">{props.modified}</span>
-=======
+import Moment from "moment";
 import ApiContext from "../ApiContext";
 import config from "../config";
 import "./Note.css";
-import Moment from "moment";
 import PropTypes from "prop-types";
 
-export default class Note extends Component {
-  static defaultProps = {
-    onDeleteNote: () => {},
-  };
-
+export default class Note extends React.Component {
   static contextType = ApiContext;
 
-  handleClickDelete = (event) => {
-    event.preventDefault();
+  handleClickDelete = (e) => {
+    e.preventDefault();
     const noteId = this.props.id;
 
-    fetch(`${config.API_endpoint}/notes/${noteId}`, {
+    fetch(`${config.API_ENDPOINT}/notes/${noteId}`, {
       method: "DELETE",
       headers: {
         "content-type": "application/json",
       },
     })
-      .then((response) => {
-        if (!response.ok) {
-          return response.json().then((error) => Promise.reject(error));
-        } else {
-          return response.json();
-        }
+      .then((res) => {
+        if (!res.ok) return res.json().then((e) => Promise.reject(e));
+        // return res.json()
       })
       .then(() => {
         this.context.deleteNote(noteId);
@@ -56,9 +31,10 @@ export default class Note extends Component {
         console.error({ error });
       });
   };
-  render() {
-    const { name, id, modified } = this.props;
 
+  render() {
+    console.log(this.context.deleteNote);
+    const { name, id, modified } = this.props;
     return (
       <div className="Note">
         <h2 className="Note_title">
@@ -79,20 +55,15 @@ export default class Note extends Component {
               {Moment(modified).format("MMM Do YYYY")}
             </span>
           </div>
->>>>>>> Stashed changes
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
-<<<<<<< Updated upstream
-//<span className="Date">{format(props.modified, "do MMM yyyy")}</span>
-=======
 Note.propTypes = {
   onDeleteNote: PropTypes.func,
   id: PropTypes.string,
   name: PropTypes.string,
   modified: PropTypes.string,
 };
->>>>>>> Stashed changes
