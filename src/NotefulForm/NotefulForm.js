01c080551336c66bdf47edf7999d7165f46d9842
Wes Glassmeyer <71471439+WesGlassmeyer@users.noreply.github.com>
2020-11-29 17:01:05 -0700

    Update NotefulForm.js

--- src/NotefulForm/NotefulForm.js
+++ src/NotefulForm/NotefulForm.js
@@ -1,10 +1,6 @@
 import React from "react";
-<<<<<<< Updated upstream
-//import './NotefulForm.css'
-=======
 import "./NotefulForm.css";
 import PropTypes from "prop-types";
->>>>>>> Stashed changes
 
 export default function NotefulForm(props) {
   const { className, ...otherProps } = props;
