8ac99578bf75b54f01856d194733144de59fe7b9
Wes Glassmeyer <wes.glassmeyer@gmail.com>
2021-01-13 19:39:29 -0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_endpoint: "http://localhost:8000",
-  //API_endpoint: "http://localhost:9090",
+  API_endpoint: `https://desolate-badlands-54923.herokuapp.com/`,
+  //API_endpoint: "http://localhost:8000"
 };
