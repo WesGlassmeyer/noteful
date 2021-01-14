d95130bbcfa4f4aa698a94386b5f7153931881e9
Wes Glassmeyer <wes.glassmeyer@gmail.com>
2021-01-13 21:20:23 -0700

    update config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
-export default {
-  API_endpoint: `https://desolate-badlands-54923.herokuapp.com/`,
+const config = {
+  API_endpoint: "https://desolate-badlands-54923.herokuapp.com/",
   //API_endpoint: "http://localhost:8000"
 };
+
+export default config;
